e55c65afb95d5b7053cc070c6d8b310f919cf12e
andresfls-buc <andresflsxx@gmail.com>
2025-06-08 14:58:17 +0900

    Initial Commit

---
+++ AprendiendoTests/AprendiendoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AprendiendoTests.swift
+//  AprendiendoTests
+//
+//  Created by Andres Landazabal on 2025/06/08.
+//
+
+import Testing
+@testable import Aprendiendo
+
+struct AprendiendoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
