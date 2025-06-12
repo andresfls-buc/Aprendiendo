f816f7cfdfb2e8c89f0457b79ab01447945cb104
andresfls-buc <andresflsxx@gmail.com>
2025-06-12 09:49:37 +0900

    Conexiones de IBOutlets y IBActions en ios con storyboard

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -8,6 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: -Referencias UI
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    @IBAction func logInButtonAction() {
+        //1. Obtener los valores de los textfield.
+        
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        
+        if email == "andres@gmail.com" && password == "123"{
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("Acceso denegado")
+        }
+    }
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
